aea76b582d588f44cd24b45ec4be527ae94bfc8d
彭涧 <2393375953@qq.com>
2018-06-02 18:36:06 +0800

    fix(build/webpack.test.config.js): In the development and environment webpack.output.library and web

--- build/webpack.test.config.js
+++ build/webpack.test.config.js
@@ -6,6 +6,8 @@ const baseConfig = require('./webpack.base.config.js');
 const config = merge(baseConfig, {
 })
 
-delete config.entry
+delete config.entry // 在开发,测试环境下 webpack.entry 的配置删除掉
+delete config.output.library // 在开发,测试环境下 webpack.output.library 的配置删除掉
+delete config.output.libraryTarget // 在开发,测试环境下 webpack.output.library 的配置删除掉
 
 module.exports = config
